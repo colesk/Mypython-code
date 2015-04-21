fc6a570cc25c2ae183f856c8877b3e884d65b252
nickidesard <nickidesard@users.noreply.github.com>
2015-04-21 14:47:08 -0400

    lists.py
    practice exercise for lists.py

---
+++ lists.py
@@ -0,0 +1,112 @@
+#####################################
+### List.py                        ##
+# Author : sravanthi kandukuri     ##
+#                                  ##
+#####################################
+
+
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+# Note: python does not have a ++ operator, but += works.
+def match_ends(words):
+    count=0
+    for s in words :
+        if len(s)>=2 and s[0]==s[-1] :
+            count=count+1
+    return ( count)
+     
+    
+
+
+
+
+ # B. front_x
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+# Hint: this can be done by making 2 lists and sorting each of them
+# before combining them.
+def front_x(words):
+    xlist=[]
+    notxlist=[]
+    for n in words :
+         if n[0] =='x':
+             xlist.append(n)
+             #print('xlist: ')
+             #print(xlist)
+         else:
+             notxlist.append(n)
+             #print('notxlist')
+             #print(notxlist)
+    #print(' JoinedList:  ')
+    sorted_xlist=sorted(xlist)
+    sorted_xlist.extend(sorted(notxlist))
+    return(sorted_xlist)
+        
+
+
+def customFn(s):
+    length=len(s)-1
+    return(s[length])
+
+    
+# C. sort_last
+# Given a list of non-empty tuples, return a list sorted in increasing
+# order by the last element in each tuple.
+# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+# Hint: use a custom key= function to extract the last element form each tuple.
+def sort_last(tuples):
+   return(sorted(tuples,key=customFn))
+ 
+       
+
+
+      
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+  return
+
+# Calls the above functions with interesting inputs.
+def main():
+
+  print ('match_ends')
+  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+  print()
+  print ('front_x')
+  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+       
+  print ()
+  print ('sort_last')
+  test(sort_last([(1, 3), (3, 2), (2, 1)]),
+       [(2, 1), (3, 2), (1, 3)])
+  test(sort_last([(2, 3), (1, 2), (3, 1)]),
+       [(3, 1), (1, 2), (2, 3)])
+  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+if __name__ == '__main__':
+  main()
+
+
+  
